a6e7d826b94f56985e909ab7503747cc8c46b36c
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2023-03-14 18:16:31 -0300

    feat: iniciando estilização do header & componentização header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-// import { useState } from 'react';
+import { useContext } from 'react';
+import MyContext from '../context/MyContext';
+import { HiSun } from "react-icons/hi";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 import '../styles/header.css'
+import Logo from './Logo';
 
 function Header() {
+   const { lightMode, setLightMode } = useContext(MyContext);
 
    const select = (el, all = false) => {
       el = el.trim()
@@ -21,31 +26,25 @@ function Header() {
       behavior: 'smooth'
     })
   }
-  const AOS = document.querySelector('header');
-  window.addEventListener('load', () => { 
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  });
+
+  // onClick={scrollto}
   return  ( 
-     <header className="container-fluid" id="header">
-      <nav id="navbar" className="navbar">
-        <ul>
-          <li><a className="nav-link scrollto active" href="#hero" onClick={scrollto}>Home</a></li>
-          </ul>
+     <header className="container-fluid backgroundHeader">
+      <nav className="container-fluid">
+      <h1 id="title"> <Logo /></h1>
+       <div className="navegation">
+          <li><a href="#about" onClick={scrollto}><span>About</span></a></li>
+          <li><a href="#skills"><span>Skills</span></a></li>
+          <li><a href="#projects"><span>Projects</span></a></li>
+          <li><a href="#contact"><span>Contact</span></a></li>
+        </div>
+        <div className="tgb" onClick={() => {setLightMode(!lightMode)}}>
+            {
+              lightMode ? <div className="tgg"><HiSun /></div>: 
+              <div className="tgs"><BsFillMoonStarsFill /></div>
+            }
+        </div>
       </nav>
-        <section className="title">
-   brenda.dev
-        </section>
-        <div className="down-arr" onClick={scrollto}>&#8595;</div>
-        <main id="main">
-      <section className="row-xs">
-        <h3>Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ut ultricies ante, eu accumsan mi. Donec mattis varius suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat lorem, mattis porta sollicitudin efficitur, maximus eu nisi. Mauris ut massa at felis tincidunt ullamcorper ac ut ligula. Proin volutpat sapien in eleifend accumsan. Pellentesque sed finibus massa.</h3>
-      </section>
-      </main>
      </header>
   );
 }
