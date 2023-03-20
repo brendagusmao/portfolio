0bc8a19896feb0f4274f42e2aee3015aec5f19c3
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2023-03-20 14:12:32 -0300

    feat: media querys about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,6 +15,7 @@ class About extends React.Component {
             <img src="https://i.pinimg.com/236x/c0/bc/7f/c0bc7f3b09d0a3deda69e5fc758d76d0.jpg" alt="" />
             </section>
             <section className="right text">
+            <img src="https://i.pinimg.com/236x/c0/bc/7f/c0bc7f3b09d0a3deda69e5fc758d76d0.jpg" alt="" className="icon-left" />
              Me definir é muito difícil. Às vezes pareço comum, às vezes singular. Sou bem assim: metamorfose ambulante. Adolescente em crise. Crises. De tudo o que você imaginar. O que mais valorizo no mundo? Amigos. O melhor sentimento? Felicidade. O melhor verbo? Amar. Conheço uma parte de uma frase, não sei o autor, mas ela define bem quem sou: viver é tentar ser feliz. É o que faço: vivo. E sim, me considero uma pessoa feliz, apesar de tudo. Depois de uma queda? Levanto e sigo em frente. Já desisti de contar os mil e um foras que dou. Vivo em busca de muitas coisas, mas já possuo a principal delas: a alegria. Uma companhia? Livros. Algo que te alegra? De novo os preciosíssimos amigos.
               <div className="tags">
               <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="" />
