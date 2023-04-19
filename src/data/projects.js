b14c69613297c69a781728845dec3fd1c2c1cbfe
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2023-04-19 16:15:20 -0300

    feat: adicionando projetos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,58 +1,61 @@
-import SolarSystem from '../image/projetcs/background (1).png';
-import PixelArt from '../image/projetcs/Pixelart.png';
-import TrybeWarts from '../image/projetcs/TrybeWarts.png';
-import Solar from '../image/projetcs/solar.gif';
+import SolarSystem from "../image/projetcs/background (1).png";
+import PixelArt from "../image/projetcs/Pixelart.png";
+import Trivia from "../image/projetcs/trivia.gif";
+import Recipes from "../image/projetcs/recipesapp3.png";
+import Solar from "../image/projetcs/solar.gif";
 // const Git = 'FaGithub';
 
 const Project = [
-    {
-      image: PixelArt,
-      name: 'Pixels Art',
-      url: 'https://link.com.br/',
-      page: 'https://link.com.br/',
-      tec: 'Frontend',
-      description: 'Projeto desenvolvido em html e css e javascript, usando componentização dom.',
-    },
-    {
-        image: TrybeWarts,
-        name: 'Trybe Warts',
-        url: 'https://link.com.br/',
-        page: 'https://link.com.br/',
-        tec: 'Frontend',
-        description: 'Feito com n sei o que',
-      },
-    {
-        image: Solar,
-        name: 'Solar System',
-        url: 'https://link.com.br/',
-        page: 'https://link.com.br/',
-        tec: 'Frontend',
-        description: 'Feito com n sei o que',
-    },
-    {
-        image: SolarSystem,
-        name: 'App Delivery',
-        url: 'https://link.com.br/',
-        page: 'https://link.com.br/',
-        tec: 'Frontend',
-        description: 'Feito com n sei o que',
-    },
-    {
-        image: SolarSystem,
-        name: 'TFC',
-        url: 'https://link.com.br/',
-        page: 'https://link.com.br/',
-        tec: 'backend',
-        description: 'Feito com n sei o que',
-    },
-    {
-        image: SolarSystem,
-        name: '.....',
-        url: 'https://link.com.br/',
-        page: 'https://link.com.br/',
-        tec: 'comming son',
-        description: 'awhait',
-    },
+  {
+    image: PixelArt,
+    name: "Pixels Art",
+    url: "https://link.com.br/",
+    page: "https://link.com.br/",
+    tec: "Frontend",
+    description:
+      "Projeto desenvolvido em html e css e javascript, usando componentização dom.",
+  },
+  {
+    image: Trivia,
+    name: "Trivia",
+    url: "https://bg-triviagame.vercel.app/",
+    page: "https://bg-triviagame.vercel.app/",
+    tec: "Frontend",
+    description:
+      "Projeto de um jogo de perguntas e respostas baseado no jogo de Trivia (semelhante ao show do milhão americano) utilizando React e Redux.",
+  },
+  {
+    image: Solar,
+    name: "Solar System",
+    url: "https://bg-solarsystem.vercel.app/",
+    page: "https://bg-solarsystem.vercel.app/",
+    tec: "Frontend",
+    description: "Landing Page feita em React, onde é possivel ter informações sobre o sistema solar",
+  },
+  {
+    image: Recipes,
+    name: "App Recipes",
+    url: "https://bg-recipesapp.vercel.app/",
+    page: "https://bg-recipesapp.vercel.app/",
+    tec: "Frontend",
+    description: "Um app de Receitas,  com filtros de busca, acompanhamento de receitas em progresso e receitas favoritas, feito com React",
+  },
+  {
+    image: SolarSystem,
+    name: "TFC",
+    url: "https://link.com.br/",
+    page: "https://link.com.br/",
+    tec: "backend",
+    description: "Feito com n sei o que",
+  },
+  {
+    image: SolarSystem,
+    name: ".....",
+    url: "https://link.com.br/",
+    page: "https://link.com.br/",
+    tec: "comming son",
+    description: "awhait",
+  },
 ];
 
 export default Project;
