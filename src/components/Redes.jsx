02069875a5ed3ea59655161ffb5714cffd927906
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2023-04-22 02:42:45 -0300

    feat: deleta email

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ImLinkedin } from 'react-icons/im';
 import { FaGithubSquare } from 'react-icons/fa';
 import { GrInstagram } from 'react-icons/gr';
-import { MdEmail } from 'react-icons/md';
+// import { MdEmail } from 'react-icons/md';
 import '../styles/utils.css';
 
 class Redes extends React.Component {
@@ -12,7 +12,6 @@ class Redes extends React.Component {
           <a href="https://www.linkedin.com/in/brendapereiragusmao/" target="_blank" rel="noreferrer" > <ImLinkedin /></a>
           <a href="https://github.com/brendagusmao" target="_blank" rel="noreferrer" > <FaGithubSquare /></a>
           <a href="https://www.instagram.com/brendapgusmao/" target="_blank" rel="noreferrer" > <GrInstagram /></a>
-          <a href="mailto:brendapgusmao@gmail.com?subject=Olá, estou entrando em contato&body=Obrigada!" target="_blank" rel="noreferrer" > <MdEmail /></a>
         </div>
     );
   }
