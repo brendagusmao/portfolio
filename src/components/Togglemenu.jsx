fb6a8ca19f0240cef77a98eec160f1f86850e6a1
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2023-04-24 19:38:29 -0300

    feat: english toggle

--- src/components/Togglemenu.jsx
+++ src/components/Togglemenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import MyContext from '../context/MyContext';
 import { BsFillGridFill, BsChevronContract } from "react-icons/bs";
 import { SiAboutdotme } from "react-icons/si";
 import { RiPagesFill } from "react-icons/ri";
@@ -8,6 +9,7 @@ import '../styles/header.css'
 
 function ToggleMenu() {
    const [menuState, setMenuState] = useState('closed');
+   const { languageMode } = useContext(MyContext);
 
    function toggleMenu() {
      setMenuState(menuState === 'closed' ? 'open' : 'closed');
@@ -30,10 +32,11 @@ function ToggleMenu() {
         </div>
           <div className="toggle">
             <ul>
-            <li><SiAboutdotme className="icontoggle" /><a onClick={toggleMenu} href="#home">home</a></li>
-            <li><SiAboutdotme className="icontoggle" /><a onClick={toggleMenu} href="#about">sobre</a></li>
-            <li><RiPagesFill className="icontoggle" /><a onClick={toggleMenu} href="#projects">projetos</a></li>
-            <li><MdPermContactCalendar className="icontoggle" /><a onClick={toggleMenu} href="#contact">contato</a></li>
+            {languageMode === 'EN' ? 
+            <><li><SiAboutdotme className="icontoggle" /><a onClick={toggleMenu} href="#home">home</a></li><li><SiAboutdotme className="icontoggle" /><a onClick={toggleMenu} href="#about">about</a></li><li><RiPagesFill className="icontoggle" /><a onClick={toggleMenu} href="#projects">projects</a></li><li><MdPermContactCalendar className="icontoggle" /><a onClick={toggleMenu} href="#contact">contact</a></li></>
+  :
+            <><li><SiAboutdotme className="icontoggle" /><a onClick={toggleMenu} href="#home">inicio</a></li><li><SiAboutdotme className="icontoggle" /><a onClick={toggleMenu} href="#about">sobre</a></li><li><RiPagesFill className="icontoggle" /><a onClick={toggleMenu} href="#projects">projetos</a></li><li><MdPermContactCalendar className="icontoggle" /><a onClick={toggleMenu} href="#contact">contato</a></li></>
+  }
             </ul>
           </div>
     </>
